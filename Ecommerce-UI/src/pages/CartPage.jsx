e57bebd5c7f0b9b9b23cd371aee1a3b7819d4d68
jake <32205542+wakejalker@users.noreply.github.com>
2024-07-11 13:01:33 -0500

    Basic skeleton of site laid out

---
+++ Ecommerce-UI/src/pages/CartPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      <p>Items in your cart will go here.</p>
+    </div>
+  );
+};
+
+export default CartPage;
---
+++ Ecommerce-UI/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import ProductList from '../components/ProductList';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to our E-Commerce Site</h1>
+      {/* <ProductList /> */}
+    </div>
+  );
+};
+
+export default HomePage;
