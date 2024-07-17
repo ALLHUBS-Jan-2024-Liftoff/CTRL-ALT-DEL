7fbd8e0ff6542599e093e4906284c5d05346140f
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-07-17 15:37:04 -0500

    Added abstract model class products

---
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/models/Products.java
@@ -0,0 +1,70 @@
+package com.project.EcommerceAppAPI.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+@MappedSuperclass
+public abstract class Products {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotBlank
+    @Size(min = 1, max = 100, message = "Characters must be between 1-100")
+    private String name;
+
+    @NotBlank
+    @Size(min = 1, max = 255, message = "Characters must be between 1-255")
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Products products = (Products) o;
+        return id == products.id && Objects.equals(name, products.name) && Objects.equals(description, products.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
