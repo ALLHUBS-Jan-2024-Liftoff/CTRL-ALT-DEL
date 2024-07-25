a0c0e4d419b42803bf88d8a9585df2d1e26b49c5
jake <32205542+wakejalker@users.noreply.github.com>
2024-07-24 22:56:23 -0500

    updated home page UI to match wireframe

---
+++ Ecommerce-UI/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>A better local marketplace</h1>
+      <p>Your neighborhood market, now digital</p>
+    <button>Shop All</button>
+    </section>
+  );
+};
+
+export default Hero;
