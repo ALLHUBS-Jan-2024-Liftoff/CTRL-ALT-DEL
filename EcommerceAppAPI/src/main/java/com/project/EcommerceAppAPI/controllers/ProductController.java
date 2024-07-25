833f94adbffe4f970a90a95d56adf805b9846dd0
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-07-25 16:17:49 -0500

    Added product Dto

--- EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/controllers/ProductController.java
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.project.EcommerceAppAPI.controllers;
 
 import com.project.EcommerceAppAPI.models.Product;
 import com.project.EcommerceAppAPI.repositories.ProductRepository;
+import com.project.EcommerceAppAPI.models.ProductCategory;
+import com.project.EcommerceAppAPI.models.dto.ProductDTO;
 import com.project.EcommerceAppAPI.repositories.ProductCategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +23,36 @@ public class ProductController {
     private ProductCategoryRepository productCategoryRepository;
 
     @GetMapping
-    public List<Product> getAllProduct(){
-        return productRepository.findAll();
+    public List<ProductDTO> getAllProduct(){
+        List<Product> products = productRepository.findAll();
+        List<ProductDTO> productDTOs = new ArrayList<>();
+        for(Product product : products) {
+            ProductDTO productDTO = new ProductDTO();
+            productDTO.setName(product.getName());
+            productDTO.setDescription(product.getDescription());
+            productDTO.setPrice(product.getPrice());
+            productDTO.setCategoryId(product.getProductCategory().getId());
+            productDTOs.add(productDTO);
+        }
+        return productDTOs;
     }
 
     @PostMapping("/new")
-    public Product createProduct(@RequestBody Product product){
-        return productRepository.save(product);
+    public ProductDTO createProduct(@RequestBody ProductDTO productDTO){
+        Product product = new Product();
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        Optional<ProductCategory> productCategoryOptional = productCategoryRepository.findById(productDTO.getCategoryId());
+        ProductCategory productCategory = productCategoryOptional.get();
+        product.setProductCategory(productCategory);
+
+        Product savedProduct =  productRepository.save(product);
+        ProductDTO savedProductDTO = new ProductDTO();
+        savedProductDTO.setName(savedProduct.getName());
+        savedProductDTO.setDescription(savedProduct.getDescription());
+        savedProductDTO.setPrice(savedProduct.getPrice());
+        savedProductDTO.setCategoryId(savedProduct.getProductCategory().getId());
+        return savedProductDTO;
     }
 }
