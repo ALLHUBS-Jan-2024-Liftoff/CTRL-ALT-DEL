54490bbc79eea7449b3ec430c426edd08306618f
Vincent Sanguinetti <vincent.m.sanguinetti@gmail.com>
2024-07-27 12:22:58 -0500

    stripe classes

--- EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/StripeAPI/PaymentService.java
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/StripeAPI/PaymentService.java
@@ -1,2 +1,16 @@
-package com.project.EcommerceAppAPI.StripeAPI;public class PaymentService {
+package com.project.EcommerceAppAPI.StripeAPI;
+
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+
+public class PaymentService {
+    public PaymentIntent createPaymentIntent(Long amount, String currency) throws Exception {
+        PaymentIntentCreateParams params =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(amount)
+                        .setCurrency(currency)
+                        .build();
+
+        return PaymentIntent.create(params);
+    }
 }
--- EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/StripeAPI/StripeConfig.java
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/StripeAPI/StripeConfig.java
@@ -1,2 +1,9 @@
-package com.project.EcommerceAppAPI.StripeAPI;public class StripeConfig {
+package com.project.EcommerceAppAPI.StripeAPI;
+
+import com.stripe.Stripe;
+
+public class StripeConfig {
+    public static void configure() {
+        Stripe.apiKey = System.getenv("STRIPE_SECRET_KEY");
+    }
 }
