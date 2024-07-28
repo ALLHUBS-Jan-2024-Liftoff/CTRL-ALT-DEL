7f0ed9157cb55ddf7bc8a5dd0982eff80f7b05d4
Shedd32 <shedd32@hotmial.com>
2024-07-28 14:11:48 -0500

    login page form

--- Ecommerce-UI/src/pages/LoginPage.jsx
+++ Ecommerce-UI/src/pages/LoginPage.jsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
       <form>
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">User Name</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+          <input type="email" class="form-control" id="exagit mpleInputEmail1" aria-describedby="emailHelp"></input>
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
