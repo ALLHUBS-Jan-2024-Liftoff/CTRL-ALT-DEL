6b27ea347b0f7450df4e690b541e30020ff5d474
2410sejal <sejal.2410@gmail.com>
2024-07-28 22:43:09 -0500

    Backend product API is updated for cors issue

--- EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/config/WebConfig.java
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/config/WebConfig.java
@@ -1,2 +1,24 @@
-package com.project.EcommerceAppAPI.config;public class WebConfig {
+package com.project.EcommerceAppAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
