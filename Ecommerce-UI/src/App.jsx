056b74913eda5fbe7a455597f15e81deea1429c8
Vincent Sanguinetti <vincent.m.sanguinetti@gmail.com>
2024-08-12 20:14:56 -0500

    update remove

--- Ecommerce-UI/src/App.jsx
+++ Ecommerce-UI/src/App.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-// import { CartProvider } from './context/CartContext.jsx';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
@@ -11,12 +8,10 @@ import HomePage from './pages/HomePage';
 import Cart from './pages/Cart';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-// import Success from './pages/Success';
-// import Cancel from './pages/Cancel';
 // import ProductDetails from './pages/ProductDetails'; // Assuming this is a page component
 import './App.css';
 
-const stripePromise = loadStripe('pk_test_51PgYz3CD9TYzROTCOsurapFheYpYoil9ZunZ3M5qOcmPwtmDJm5rvCYA7EzIrrbR7G7M4VZbTt5kZogkTdMwG9jV00cAwnjVNd');
+
 
 import FeaturedProduct from './components/FeaturedProduct';
 import ProductsPage from './pages/ProductsPage';
@@ -29,22 +24,6 @@ import CategoryList from './components/CategoryList';
 import Sellers from './components/Sellers';
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
-    const query = new URLSearchParams(window.location.search);
-
-    if (query.get("success")) {
-      setMessage("Order placed! You will receive an email confirmation.");
-    }
-
-    if (query.get("canceled")) {
-      setMessage(
-        "Order canceled -- continue to shop around and checkout when you're ready."
-      );
-    }
-  }, []);
 
   return (
       <Router>
@@ -66,8 +45,7 @@ function App() {
           <Footer />
         </div>
 
-        {/* Render the checkout message or product display
-        {message ? <Message message={message} /> : <ProductDisplay />} */}
+
       </Router>
   );
 }
