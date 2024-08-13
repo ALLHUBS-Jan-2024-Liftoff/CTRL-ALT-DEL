0172f635f6a86e788472d00dd5266cf74062a43d
2410Sejal <sejal.2410@gmail.com>
2024-08-13 13:43:50 -0500

    All the CRUD functionality up and running

--- EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/controllers/ProductCategoryController.java
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/controllers/ProductCategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/productCategory")
@@ -22,5 +23,34 @@ public class ProductCategoryController {
     public ProductCategory createCategory(@RequestBody ProductCategory productCategory){
         return productCategoryRepository.save(productCategory);
     }
+
+    @PutMapping("/{categoryID}")
+    public ProductCategory fetchCategory(@PathVariable int categoryID) {
+        Optional<ProductCategory> optionalCategory = productCategoryRepository.findById(categoryID);
+        if (optionalCategory.isPresent()){
+            return optionalCategory.orElse(null);
+        } else {
+            return null;
+        }
+    }
+    @PostMapping("/delete")
+    public void deleteCategory (@RequestParam int categoryId) {
+        productCategoryRepository.deleteById(categoryId);
+    }
+
+    @PostMapping("/update")
+    public ProductCategory updateProductCategory(@RequestBody ProductCategory productCategory){
+        Optional<ProductCategory> optionalCategory = productCategoryRepository.findById(productCategory.getId());
+
+        ProductCategory productCategoryDb = optionalCategory.orElse(null);
+        if(productCategoryDb!=null) {
+            productCategoryDb.setName(productCategory.getName());
+            productCategoryDb.setDescription(productCategory.getDescription());
+            return productCategoryRepository.save(productCategoryDb);
+        } else {
+            return null;
+        }
+    }
+
 
 }
