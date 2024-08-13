2f1ab93b8d0f4d848b09a825e250d35b48b1f503
Shedd32 <shedd32@hotmial.com>
2024-08-12 20:50:13 -0500

    replaced all code

--- Ecommerce-UI/src/components/Header.jsx
+++ Ecommerce-UI/src/components/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom';
+import axiosInstance from '../services/axiosService';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+        await axiosInstance.post('/logout');
+        localStorage.setItem('loggedIn', false);
+        navigate('/login'); // Redirect to login page
+    } catch (error) {
+        console.error("Logout failed", error);
+    }
+};
 
   return (
     <header className="header">
@@ -13,9 +26,12 @@ const Header = () => {
       <nav className="navigation">
         <ul className="nav-list">
           <li><Link to ="/products">Shop</Link></li>
-          <li><Link to ="/sellers">Sellers</Link></li>
+          <li><Link to ="/createProduct">Sellers</Link></li>
           <li><Link to ="/about">About</Link></li>
-          <li><Link to ="/login">Login</Link></li>
+          <li>{localStorage.getItem('loggedIn') === 'true' ? (
+              <Link onClick={handleLogout}>Logout</Link>
+                 ) : (<Link to="/login">Login</Link>)}
+          </li>
         </ul>
       </nav>
       
