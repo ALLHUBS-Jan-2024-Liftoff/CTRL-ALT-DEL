26d4aafe5d43c0b79f596406a9048b7f3c2e7b1c
jake <32205542+wakejalker@users.noreply.github.com>
2024-08-16 16:08:39 -0500

    created review repository

---
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/repositories/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.project.EcommerceAppAPI.repositories;
+
+import com.project.EcommerceAppAPI.models.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByProductId(long productId);
+}
