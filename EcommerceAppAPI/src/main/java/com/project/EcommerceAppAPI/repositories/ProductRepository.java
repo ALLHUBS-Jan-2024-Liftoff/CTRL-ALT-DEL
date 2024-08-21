178a322dc2201866e8db431a3cc07aaf270b6f55
2410Sejal <sejal.2410@gmail.com>
2024-08-21 15:25:23 -0500

    Added all necessery changes all features working

--- EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/repositories/ProductRepository.java
+++ EcommerceAppAPI/src/main/java/com/project/EcommerceAppAPI/repositories/ProductRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-
     List<Product> findByNameContainingIgnoreCase(String name);
-
 }
+
