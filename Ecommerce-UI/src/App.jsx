fd4e94dbbd53124b05bede5509c9f79703733750
Vincent Sanguinetti <78439593+vsangui2@users.noreply.github.com>
2024-08-22 17:17:46 -0500

    Update App.jsx

--- Ecommerce-UI/src/App.jsx
+++ Ecommerce-UI/src/App.jsx
@@ -5,11 +5,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import Cart from './pages/Cart';
-import ChatPage from './pages/ChatPage';
 import CheckoutForm from './components/CheckoutForm';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProductsPage from './pages/ProductsPage';
+import ChatPage from './pages/ChatPage';
 import ProductForm from './components/ProductForm';
 import CategoryForm from './components/CategoryForm';
 import CategoryList from './components/CategoryList';
@@ -24,51 +24,51 @@ import About from './components/About';
 import { CartProvider, useCart } from './components/CartProvider'; 
 
 function App() {
- 
+  return (
+    <CartProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </CartProvider>
+  );
+}
 
 function AppContent() {
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('loggedIn') === 'true');
   const { addToCart, updateCartItemQuantity, removeCartItem, cartItems } = useCart();
 
-   return (
-    <CartProvider>
-      <Router>
-
-        <div>
-        <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductsPage onAddToCart={handleAddToCart} />} />
-            <Route path="/newProduct" element={<ProductForm />} />
-            <Route path="/manageProducts" element={<ManageProducts />} />
-            <Route path="/updateProduct/:id" element={<ProductUpdateForm />} />
-            <Route path="/productDetails/:id" element={<ProductDetails onAddToCart={handleAddToCart} />} />
-            <Route path="/sellers" element={<Sellers />} />
-            <Route path="/listCategories" element={<CategoryList />} />
-            <Route path="/newCategory" element={<CategoryForm />} />
-            <Route path="/cart" element={
-              <Cart 
-                cartItems={cartItems} 
-                updateCartItemQuantity={updateCartItemQuantity} 
-                removeCartItem={removeCartItem} 
-              />
-            } />
-            <Route path="/search" element={<SearchProduct onAddToCart={handleAddToCart}/>} />
-            <Route path="/checkout" element={<CheckoutForm cartItems={cartItems} />} />
-            <Route path="/success" element={<SuccessPage />} />
-            <Route path="/cancel" element={<CancelPage />} />
-            <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/ChatPage" element={<ChatPage setIsLoggedIn={setIsLoggedIn} />} />
-          </Routes>
-          <Footer isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-        </div>
-
-        <AppContent />
-
-      </Router>
-    </CartProvider>
+  return (
+    <>
+      <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<ProductsPage onAddToCart={addToCart} />} />
+        <Route path="/newProduct" element={<ProductForm />} />
+        <Route path="/manageProducts" element={<ManageProducts />} />
+        <Route path="/updateProduct/:id" element={<ProductUpdateForm />} />
+        <Route path="/productDetails/:id" element={<ProductDetails onAddToCart={addToCart} />} />
+        <Route path="/sellers" element={<Sellers />} />
+        <Route path="/listCategories" element={<CategoryList />} />
+        <Route path="/newCategory" element={<CategoryForm />} />
+        <Route path="/cart" element={
+          <Cart 
+            cartItems={cartItems} 
+            updateCartItemQuantity={updateCartItemQuantity} 
+            removeCartItem={removeCartItem} 
+          />
+        } />
+        <Route path="/search" element={<SearchProduct onAddToCart={addToCart}/>} />
+        <Route path="/checkout" element={<CheckoutForm cartItems={cartItems} />} />
+        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/cancel" element={<CancelPage />} />
+        <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/ChatPage" element={<ChatPage setIsLoggedIn={setIsLoggedIn} />} />
+      </Routes>
+      <Footer isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+    </>
   );
 }
-      
+
 export default App;
